1c08588a551acf544e172306f0100434fd9451da
Rajat Singhal <rajat0627@gmail.com>
2018-11-13 15:55:13 +0530

    init

---
+++ BinaryTree.java
@@ -0,0 +1,59 @@
+class Node {
+
+    int item;
+    Node left_child;
+    Node right_child;
+
+    public Node(int item) {
+        this.item = item;
+    }
+}
+
+class BinaryTree {
+
+    Node root;
+
+    public void traverseBreadthFirst() {
+
+    }
+
+    public void traverseDepthFirstInorder() { //Left-Root-Right
+
+    }
+
+    public void traverseDepthFirstPreorder() { // Root-Left-Right
+
+    }
+
+    public void traverseDepthFirstPostorder() { // Left-Right-Root
+
+    }
+
+    public int calculateHeight() {
+
+    }
+
+    public void mirror() {
+
+    }
+
+}
+
+class BinarySearchTree extends BinaryTree {
+
+    void insert(int item) {
+
+    }
+
+    bool search(int item) {
+
+    }
+
+    void delete(int item) {
+
+    }
+}
+
+class BalancedBinarySearchTree extends BinarySearchTree {
+    
+}
