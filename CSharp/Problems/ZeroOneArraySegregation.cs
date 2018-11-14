6a9211c311abfd159f3d4d264f7c86eed072f689
Rajat Singhal <rajat0627@gmail.com>
2018-11-14 21:35:40 +0530

    bug_fix_and_test_case

--- CSharp/Problems/ZeroOneArraySegregation.cs
+++ CSharp/Problems/ZeroOneArraySegregation.cs
@@ -1,21 +1,20 @@
 namespace CSharp.Problems {
     public class ZeroOneArraySegregation {
-        
-        //public static int[] input = { 0, 1, 0, 1, 0, 0, 1, 1, 1, 0 };
 
         public static int[] segregate(int[] input) {
             int next_rear_index = input.Length - 1;
             for(int loop_index = 0; loop_index < input.Length; loop_index++) {
+                if (next_rear_index == loop_index)
+                    break;
                 if(input[loop_index] == 1) {
                     while(input[next_rear_index] == 1) {
                         next_rear_index--;
                         if(next_rear_index == loop_index)
                             break;
                     }
-                    if (next_rear_index == loop_index)
-                        break;
                     input[next_rear_index] = 1;
                     input[loop_index] = 0;
+                    next_rear_index--;
                 }
             }
             return input;
--- CSharp/TestDSA.cs
+++ CSharp/TestDSA.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using CSharp.LinkedList;
+using CSharp.Problems;
 using System.Collections.Generic;
 
 namespace CSharp{
@@ -15,9 +16,17 @@ namespace CSharp{
             Console.WriteLine("Resultant Doubly Linked List: " + String.Join(" ", dll.traverse()));
         }
 
+        static void testZeroOneArraySegregation() {
+            int[] input = { 0, 1, 0, 1, 0, 0, 1, 1, 0 };
+            Console.WriteLine("Input: " + String.Join(" ", input));
+            int[] output = ZeroOneArraySegregation.segregate(input);
+            Console.WriteLine("Output: " + String.Join(" ", output));
+        }
+
         public static void Main(string[] args) {
             Console.WriteLine("Starting Test Suite!!");
-            testDoublyLinkedList();
+            //testDoublyLinkedList();
+            testZeroOneArraySegregation();
         }
     }
 }
