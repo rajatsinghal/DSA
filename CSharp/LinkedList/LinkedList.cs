1549c1328a951ad38e87f8dd2f782acf9c50ef87
Rajat Singhal <rajat0627@gmail.com>
2018-11-15 12:34:31 +0530

    detectAndRemoveLoop_untested

--- CSharp/LinkedList/LinkedList.cs
+++ CSharp/LinkedList/LinkedList.cs
@@ -24,8 +24,39 @@ namespace CSharp.LinkedList {
             head = new_node;
         }
 
-        public void detectAndRemoveLoop() {
-            
+        public bool detectAndRemoveLoop() {
+            Node fast_node = head;
+            Node slow_node = head;
+            bool has_loop = false;
+            while(fast_node != null && fast_node.next != null) {
+                slow_node = slow_node.next;
+                fast_node = fast_node.next.next;
+                if(slow_node == fast_node) {
+                    has_loop = true;
+                    break;
+                }
+            }
+            if(!has_loop) {
+                return false;
+            } else { //Remove loop
+                HashSet<Node> loop_nodes = new HashSet<Node>();
+                Node iterative_loop_node = slow_node;
+                loop_nodes.Add(iterative_loop_node);
+                while(iterative_loop_node.next != slow_node) {
+                    iterative_loop_node = iterative_loop_node.next;
+                    loop_nodes.Add(iterative_loop_node);
+                }
+                Node iterative_head = head;
+                while(!loop_nodes.Contains(iterative_head)) {
+                    iterative_head = iterative_head.next;
+                }
+                Node loop_start_node = iterative_head;
+                while(iterative_head.next != loop_start_node) {
+                    iterative_head = iterative_head.next;
+                }
+                Node loop_end_node = iterative_head;
+                loop_end_node.next = null; //Loop removed
+            }
         }
 
     }
