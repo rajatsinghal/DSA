af0e99cc4f015a4fd54d990c9470bdedfc5c02cc
Rajat Singhal <rajat0627@gmail.com>
2018-11-29 14:23:36 +0530

    start

---
+++ CSharp/Sort/KSortedArray.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace CSharp.Sort {
+    class KSortedArray {
+
+        public static int[] sort(int[] items) {
+            return items;
+        }
+
+    }
+}
