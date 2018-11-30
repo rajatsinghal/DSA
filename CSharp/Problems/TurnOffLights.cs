3cb3e02d335885c9e64039f0f7368682cb0eca55
Rajat Singhal <rajat0627@gmail.com>
2018-11-30 16:26:48 +0530

    untested

---
+++ CSharp/Problems/TurnOffLights.cs
@@ -0,0 +1,55 @@
+using System;
+
+namespace CSharp.Problems {
+    //https://codeforces.com/problemset/problem/812/B
+    class TurnOffLights {
+
+        int[,] floor_plan;
+        int[] left_most_lights;
+        int[] right_most_lights;
+
+        public TurnOffLights(int[,] floor_plan) {
+            this.floor_plan = floor_plan;
+            this.left_most_lights = new int[floor_plan.GetLength(0)];
+            this.right_most_lights = new int[floor_plan.GetLength(0)];
+        }
+
+        public int solve() {
+            for(int i = 0; i < floor_plan.GetLength(0); i++) {
+                left_most_lights[i] = 0;
+                right_most_lights[i] = 0;
+                for(int j = 1; j < floor_plan.GetLength(1) - 1; j++) { //First and Last column are staris
+                    if(floor_plan[i, j] == 1) {
+                        if(left_most_lights[i] == 0) {
+                            left_most_lights[i] = j;
+                        }
+                        right_most_lights[i] = j;
+                    }
+                }
+            }
+            return solveFromFloorIndex(0, 'L');
+        }
+
+        public int solveFromFloorIndex(int floor_index, char enter_direction) {
+            int left_exit_time;
+            int right_exit_time;
+            if(enter_direction == 'L') {
+                left_exit_time = right_most_lights[floor_index] * 2;
+                right_exit_time = floor_plan.GetLength(1) - 1;
+            } else {
+                right_exit_time = left_most_lights[floor_index] * 2;
+                left_exit_time = floor_plan.GetLength(1) - 1;
+            }
+
+            if(floor_index == floor_plan.GetLength(0) - 1) {
+                return Math.Max(left_exit_time, right_exit_time);
+            } else {
+                return Math.Max(
+                    (left_exit_time + solveFromFloorIndex(floor_index - 1, 'L')), 
+                    (right_exit_time + solveFromFloorIndex(floor_index - 1, 'R'))
+                );
+            }
+        }
+
+    }
+}
