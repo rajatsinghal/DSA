8dc5545ca3e245d1d727a020dcc7c2a96cec12e7
Rajat Singhal <rajat0627@gmail.com>
2018-12-04 08:32:43 +0530

    untested

---
+++ CSharp/Tree/BinaryTree.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace CSharp.Tree {
+    class BinaryFree {
+
+        class Node : TreePrinter.PrintableNode {
+            public int item;
+            public Node left_child;
+            public Node right_child;
+
+            public Node(int item) {
+                this.item = item;
+            }
+
+            public override TreePrinter.PrintableNode getLeft() {
+                return left_child;
+            }
+
+            public override TreePrinter.PrintableNode getRight() {
+                return right_child;
+            }
+
+            public override string getText() {
+                return "" + item;
+            }
+
+            public void printInPreOrder() {
+                System.Console.WriteLine(item + "");
+                if (left_child != null)
+                    left_child.printInPreOrder();
+                if (right_child != null)
+                    right_child.printInPreOrder();
+            }
+
+            public int getMinDepth() {
+                int left_child_depth = this.left_child != null ? this.left_child.getMinDepth() : 0;
+                int right_child_depth = this.right_child != null ? this.right_child.getMinDepth() : 0;
+                return Math.Max(left_child_depth, right_child_depth) + 1;
+            }
+        }
+
+        Node root;
+
+        public int getMinDepth() {
+            return this.root.getMinDepth();
+        }
+
+    }
+}
